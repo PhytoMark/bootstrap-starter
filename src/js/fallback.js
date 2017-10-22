a69095c3f335611ecc37cadca57992a8b893ceeb
Lai Weng Han <jlwhoo7@gmail.com>
2017-10-22 22:29:34 +0800

    [CDN] Added CDN

---
+++ src/js/fallback.js
@@ -0,0 +1,3 @@
+    window.jQuery || document.write('<script src="js/jquery.min.js">\x3C/script>');
+    window.Popper || document.write('<script src="js/popper.min.js">\x3C/script>');
+    window.bootstrap || document.write('<script src="js/bootstrap.min.js">\x3C/script>');
